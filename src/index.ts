07d7a1ed802217e10a07f8411a1308a1a1679864
SURFACEPRO9\mitsu <mitsuhiro.seino@gmail.com>
2026-05-08 20:00:05 +0900

    style-proxy

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,33 @@
-export type * from './types';
+export type {
+  DeferRenderingResult,
+  DeferRenderingWithHandlersResult,
+  RenderingState,
+} from './types';
 export { default as useDeferUntilAsyncComplete } from './useDeferUntilAsyncComplete';
+export type { UseDeferUntilAsyncCompleteOptions } from './useDeferUntilAsyncComplete';
 export { default as useDeferUntilBreakpoint } from './useDeferUntilBreakpoint';
+export type { UseDeferUntilBreakpointOptions } from './useDeferUntilBreakpoint';
 export { default as useDeferUntilCalledCount } from './useDeferUntilCalledCount';
+export type { UseDeferUntilCalledCountOptions } from './useDeferUntilCalledCount';
 export { default as useDeferUntilChange } from './useDeferUntilChange';
+export type { UseDeferUntilChangeOptions } from './useDeferUntilChange';
 export { default as useDeferUntilDate } from './useDeferUntilDate';
+export type { UseDeferUntilDateOptions } from './useDeferUntilDate';
 export { default as useDeferUntilFontReady } from './useDeferUntilFontReady';
+export type { UseDeferUntilFontReadyOptions } from './useDeferUntilFontReady';
 export { default as useDeferUntilIntersected } from './useDeferUntilIntersected';
+export type { UseDeferUntilIntersectedOptions } from './useDeferUntilIntersected';
 export { default as useDeferUntilOnReady } from './useDeferUntilOnReady';
+export type { UseDeferUntilOnReadyOptions } from './useDeferUntilOnReady';
 export { default as useDeferUntilReady } from './useDeferUntilReady';
+export type { UseDeferUntilReadyOptions } from './useDeferUntilReady';
 export { default as useDeferUntilRender } from './useDeferUntilRender';
+export type { UseDeferUntilRenderOptions } from './useDeferUntilRender';
 export { default as useDeferUntilResolved } from './useDeferUntilResolved';
+export type { UseDeferUntilResolvedOptions } from './useDeferUntilResolved';
 export { default as useDeferUntilScrolled } from './useDeferUntilScrolled';
+export type { UseDeferUntilScrolledOptions } from './useDeferUntilScrolled';
 export { default as useDeferUntilTimeout } from './useDeferUntilTimeout';
+export type { UseDeferUntilTimeoutOptions } from './useDeferUntilTimeout';
 export { default as useDeferUntilTrue } from './useDeferUntilTrue';
+export type { UseDeferUntilTrueOptions } from './useDeferUntilTrue';
